9fdc29bf5ed9ecc14a769a2417b6d0633fc27db9
Sjors van Gelderen <sjors@openmailbox.org>
2016-09-20 11:57:18 +0200

    Modified turtle assignment code for compatibility with terminals

--- code/assignment 4/turtle/End.py
+++ code/assignment 4/turtle/End.py
@@ -1,6 +1,5 @@
-﻿# Step 1: Make all the "turtle" commands available to us.
+﻿# Make all the "turtle" commands available to us.
 import turtle
 
-
-# do not remove this
+# End the turtle functionality
 turtle.done()
--- code/assignment 4/turtle/Start.py
+++ code/assignment 4/turtle/Start.py
@@ -1,65 +1,35 @@
-﻿import turtle
-from turtle import Vec2D
+﻿"""
+Defines functionality for the turtle assignment
+"""
 
+import turtle
+from time import sleep
 
-t = turtle.Turtle()
-
-
-def down():
-    t._orient = Vec2D(1.0, 1.0)
-
-
-def forward(n):
-    t.forward(n * 10)
-
-
-class Direction:
-    Up = 1
-    Down = 2
-    Left = 3
-    Right = 4
-
-
-class Color:
-    Red = "red"
-    Blue = "blue"
-    Black = "black"
-    Green = "green"
-
-Red = Color.Red
-Blue = Color.Blue
-Black = Color.Black
-Green = Color.Green
-
-
-def change_color_to(color):
-    t.pencolor(color)
-
-
-def turn(n):
-    if not (type(n) is Direction.Up):
-        t._rotate(n * -1)
-    else:
-        if n == Direction.Up:
-            t._orient = Vec2D(0, 1.0)
-        if n == Direction.Left:
-            t._orient = Vec2D(-1.0, 0)
-        if n == Direction.Down:
-            t._orient = Vec2D(0, -1.0)
-        if n == Direction.Right:
-            t._orient = Vec2D(1.0, 0)
-
-
+# Stores ASCII keycodes
 keys = []
 
+# Callback for Turtle input
 def keypress(e):
     keys.append(e.char)
 
+# Create turtle context
+t = turtle.Turtle()
 s = turtle.Screen()
 s._root.bind("<KeyPress>", keypress)
 
-from time import sleep
+# Move the pen forward
+def forward(n):
+    t.forward(n * 10)
 
+# Turn the pen
+def turn(n):
+    t._rotate(n * -1)
+
+# Change the ink color
+def change_color_to(color):
+    t.pencolor(color)
+
+# Get input ASCII keycodes
 def get():
     while len(keys) == 0:
         try:
@@ -69,29 +39,8 @@ def get():
         sleep(0.1)
     c = ord(keys.pop(0))
     return c
-
-# def get():
-#     from msvcrt import getch
-#     c = ord(getch())
-#     ## On a Mac or Unix, you can uncomment the lines below
-#     ## and comment the two lines above.
-#     #import termios, sys, os
-#     #TERMIOS = termios
-#     #fd = sys.stdin.fileno()
-#     #old = termios.tcgetattr(fd)
-#     #new = termios.tcgetattr(fd)
-#     #new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
-#     #new[6][TERMIOS.VMIN] = 1
-#     #new[6][TERMIOS.VTIME] = 0
-#     #termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
-#     #c = None
-#     #try:
-#     #    c = ord(os.read(fd, 1))
-#     #finally:
-#     #    termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
-#     return c
-
-
-def run(a):
+    
+# Run the program
+def run(program):
     while True:
-        a()
+        program()
